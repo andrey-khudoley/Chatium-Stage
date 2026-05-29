aca4a5b3ff37f53688fee3805f11e277bb8654a4
Andrey Khudoley <andrey@khudoley.pro>
2026-05-30 01:42:16 +0300

    fix(gw-gc): включил операцию get-offers
    - availability: 'disabled' → 'enabled' для /offer/get-offers в operationsCatalogNew1

--- p/saas/gw/gc/lib/gateway/operationsCatalogNew1.ts
+++ p/saas/gw/gc/lib/gateway/operationsCatalogNew1.ts
@@ -350,7 +350,7 @@ export const operationsCatalogNew1: OperationEntry[] = [
     contour: 'new',
     httpMethod: 'GET',
     pathTemplate: '/offer/get-offers',
-    availability: 'disabled',
+    availability: 'enabled',
     legacyImportAction: null,
     argsValidator: s.object({}),
     argsSchema: EMPTY_SCHEMA
